b35babdd6309e4ee85fd467c273b7cf478071ec6
Maurício Leite <mauricioleite1@gmail.com>
2025-05-05 19:44:41 -0300

    feat: adjustments to style in general

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import CompanyInput from '@/components/company-input';
+import CompanyProfileCardsSection from '@/components/company-profile-cards-section';
 
 export default function Home() {
   return (
-    <main className='flex h-[100dvh] w-[100dvw] flex-col items-center justify-center gap-y-8'>
-      <h1>Company Profile AI</h1>
-      <CompanyInput />
+    <main className='flex h-[100dvh] w-[100dvw] flex-col items-center justify-center gap-y-24'>
+      <section className='flex flex-col items-center justify-center gap-y-8'>
+        <div className='flex w-full flex-col items-center justify-center'>
+          <h1 className='bg-gradient-to-br from-neutral-900 via-neutral-900 to-emerald-500 bg-clip-text text-4xl font-bold text-transparent'>
+            Company Profile AI
+          </h1>
+
+          <p className='text-neutral-400'>
+            A company profile generator powered by AI
+          </p>
+        </div>
+        <CompanyInput />
+      </section>
+
+      <CompanyProfileCardsSection />
     </main>
   );
 }
--- components/company-input.tsx
+++ components/company-input.tsx
@@ -2,20 +2,22 @@
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { ArrowUp } from 'lucide-react';
+import { ArrowRight, ArrowUp } from 'lucide-react';
 
 export default function CompanyInput() {
   return (
-    <div className='relative w-120 min-w-80'>
+    <div className='relative w-140 min-w-100'>
       <Input
         placeholder='Insert a company url..'
         type='text'
         onChange={(e) => {
           console.log(e.target.value);
         }}
+        className='pr-44'
       />
-      <Button className='absolute top-1/2 right-2 -translate-y-1/2'>
+      <Button className='absolute top-1/2 right-2 -translate-y-1/2 bg-emerald-500 text-white ease-in-out hover:bg-emerald-500/80'>
         <ArrowUp />
+        Generate Profile
       </Button>
     </div>
   );
