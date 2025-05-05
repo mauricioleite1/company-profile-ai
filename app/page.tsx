606c47d4c9cb800fe517b219806579fabada29fb
Maurício Leite <mauricioleite1@gmail.com>
2025-05-05 15:58:36 -0300

    feat: initial layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import CompanyInput from '@/components/company-input';
 
 export default function Home() {
   return (
-    <main className='flex flex-col h-[100dvh] w-[100dvw] items-center justify-center'>
+    <main className='flex h-[100dvh] w-[100dvw] flex-col items-center justify-center gap-y-8'>
       <h1>Company Profile AI</h1>
-      <div className='min-w-80 w-200'>
-        <CompanyInput />
-      </div>
+      <CompanyInput />
     </main>
   );
 }
