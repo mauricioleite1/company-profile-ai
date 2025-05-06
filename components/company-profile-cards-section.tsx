c075c991f95dbc8613b55f018529a0df38babcee
Maurício Leite <mauricioleite1@gmail.com>
2025-05-06 10:48:32 -0300

    update

--- components/company-profile-cards-section.tsx
+++ components/company-profile-cards-section.tsx
@@ -8,7 +8,7 @@ export default function CompanyProfileCardsSection() {
 
   return (
     <section className='from-background mb-1 flex min-h-24 flex-wrap items-start justify-start gap-6 overflow-y-auto rounded-xl bg-gradient-to-t to-transparent p-1 px-4 md:w-[860px] md:min-w-[400px]'>
-      {data.reverse().map((company, i) => {
+      {data.map((company, i) => {
         if (
           company.company_name === '' ||
           /not found/i.test(company.company_name)
