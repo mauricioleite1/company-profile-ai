76a9ab6ba1865543a692881fcc29545a9cfacbf3
Maurício Leite <mauricioleite1@gmail.com>
2025-05-05 21:48:37 -0300

    feat: input-shortcuts added as example on home page

---
+++ components/input-shortcut.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import useInputStore from '@/stores/use-input-store';
+
+interface InputShortcutProps {
+  text: string;
+}
+
+export default function InputShortcut({ text }: InputShortcutProps) {
+  const { domain, setDomain } = useInputStore();
+
+  const handleClick = () => {
+    setDomain(text);
+    const input = document.querySelector(
+      'input[name="company"]',
+    ) as HTMLInputElement;
+    if (input) {
+      input.value = text;
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+      input.focus();
+    }
+  };
+
+  return (
+    <Button
+      className='bg-background border border-emerald-300 text-neutral-600 hover:-translate-y-[2px] hover:bg-transparent hover:shadow'
+      onClick={handleClick}
+    >
+      {text}
+    </Button>
+  );
+}
