6b944fe1d30654b429d064f0cb83fd358e503d51
Maurício Leite <mauricioleite1@gmail.com>
2025-05-06 07:45:44 -0300

    refactor: cleaning code and restructuring

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,11 +18,11 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost:
-          'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+          'hover:bg-accent hover:text-green-300 dark:hover:bg-green-300/50',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
