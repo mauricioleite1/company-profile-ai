681e7e85694dd453aaadba2fb256c07d6bdbdaa5
Maurício Leite <mauricioleite1@gmail.com>
2025-05-06 06:46:10 -0300

    refact: cleaning company-profile-cards-section.tsx

--- components/company-profile-cards-section.tsx
+++ components/company-profile-cards-section.tsx
@@ -1,21 +1,14 @@
 'use client';
 
 import CompanyProfileCard from '@/components/company-profile-card';
-import { mock } from '@/lib/mock';
-import useDataStore from '@/stores/use-data-store';
-import { useEffect } from 'react';
+import useSortData from '@/hooks/use-sort-data';
 
 export default function CompanyProfileCardsSection() {
-  const { data, setData } = useDataStore();
-  const companies = mock;
-
-  useEffect(() => {
-    setData(companies);
-  }, [companies]);
+  const { sortedData } = useSortData();
 
   return (
     <section className='from-background flex min-h-24 w-[860px] min-w-[900px] flex-wrap items-center justify-start gap-6 overflow-y-auto rounded-xl bg-gradient-to-t to-transparent p-1 pb-12'>
-      {data.map((company, i) => (
+      {sortedData.map((company, i) => (
         <CompanyProfileCard key={i} company={company} />
       ))}
     </section>
