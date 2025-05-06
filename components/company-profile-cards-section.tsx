aba1571e17b87ff2fd14a089722abe63bf3a9439
Maurício Leite <mauricioleite1@gmail.com>
2025-05-06 08:57:43 -0300

    feat: add not found validation

--- components/company-profile-cards-section.tsx
+++ components/company-profile-cards-section.tsx
@@ -9,7 +9,10 @@ export default function CompanyProfileCardsSection() {
   return (
     <section className='from-background flex min-h-24 w-[860px] min-w-[900px] flex-wrap items-center justify-start gap-6 overflow-y-auto rounded-xl bg-gradient-to-t to-transparent p-1 pb-12'>
       {sortedData.map((company, i) => {
-        if (company.company_name === '' || company.company_name === 'Not found')
+        if (
+          company.company_name === '' ||
+          /not found/i.test(company.company_name)
+        )
           return null;
         return <CompanyProfileCardContainer key={i} company={company} />;
       })}
